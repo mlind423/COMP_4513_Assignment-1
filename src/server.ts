bc55828ee70fadf8bf961ccfc3b7a552ddda1a8d
mlind423 <mlind423@mtroyal.ca>
2024-03-20 11:59:48 -0600

    Update

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express, {Request, Response} from 'express';
 const supa = require('@supabase/supabase-js');
 const app = express();
 const port:number = 3000;
-const supaURL:string = 'https://nfkqglfqiwxkxbxinyom.supabase.co';
-const supaAnonKey:string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ma3FnbGZxaXd4a3hieGlueW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY1NjA0NTIsImV4cCI6MjAyMjEzNjQ1Mn0.xMPY2ZSGL7318ueDg1IBOQvUhqUR0Sydj_Eh3EYKuqI';
+const supaURL = process.env.SUPABASE_URL;
+const supaAnonKey = process.env.SUPABASE_KEY;
 
 const supabase = supa.createClient(supaURL, supaAnonKey);
 
